3731bf06d5ccc902c304cfb9df2292bad2c811cd
itslucassouza <lucasvini193@hotmail.com>
2024-01-12 18:15:44 -0300

    initial commit

---
+++ memorized/memorized/memorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizedApp.swift
+//  memorized
+//
+//  Created by Lucas Vinicius Ramos Souza on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct memorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
